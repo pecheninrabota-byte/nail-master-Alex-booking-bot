d439ede38786ee7a4da32697f0fecdb83c5f5179
Печенин Сергей <pecheninrabota@gmail.com>
2026-04-20 11:30:53 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ if engine is not None:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
